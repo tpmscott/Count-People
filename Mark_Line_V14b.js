41f0489ae99664927f94a5fe7f06b4fd7ae9161a
tpmscott <49225761+tpmscott@users.noreply.github.com>
2021-01-06 20:58:22 +1000

    Add files via upload

--- Mark_Line_V14b.js
+++ Mark_Line_V14b.js
@@ -282,6 +282,23 @@ async function init_Bible4U_DB() {
          Comparing_Db_1: 'CNo',
          Comparing_Db_2: 'CNo'
      });
+     dbT2.version(25).stores({      // New for V13
+         books: 'name,date',
+         ChapNote: 'name,date',
+         Congregation: 'CNo,F_Name,L_Name,[L_Name+F_Name],H_No,F_Order',
+         Attendance: 'Record',
+         Householder: '++H_Seq,H_No',
+         Congregation2: '++CNo,F_Name,L_Name,[L_Name+F_Name],H_No,F_Order',
+         Congregation3: 'CNo,F_Name,L_Name,[L_Name+F_Name],H_No',
+         Roll: '++Roll_No,CNo,F_Name,L_Name,C_F_Name,H_No,E_F_LName,C_F_FName,Member',
+         Congregation4: 'CNo,F_Name,L_Name,[L_Name+F_Name],H_No,F_Order,Tel,Mob',
+         SermonNote: 'Name,Speaker, *MainVerses, *KeyWords',
+         Pictures: 'ID, P_Name, *MainVerses',
+         Service_Record: 'ID_1,ID_2',
+         Comparing_Db_1: 'CNo',
+         Comparing_Db_2: 'CNo',
+         Bible_Db_1: 'Verse_Content'
+     });
 
    // End of Declare Database
 
@@ -5749,7 +5766,7 @@ async function Roll_Call_UPC() {  // Arg: 'R' for Roll Call, 'P' for Phone Dir.
 
   //var kindy_member = new Array(93,102,95); // 93 Hazel Chen
 
-  var UPC_member = new Array(146,106,251,72,168,24,125,16,52,137,98,42,15,278,279,'CIC139',109,244,38,76,11,49,51,97); // ,139
+  var UPC_member = new Array(146,106,251,72,168,24,125,16,52,137,98,42,15,279,'CIC139',109,244,38,76,11,49,51,97); // ,139
 
  
   //      table.where(indexOrPrimKey).anyOf(array)
